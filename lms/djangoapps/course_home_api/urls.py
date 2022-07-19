1209924974dbf11a969dede057da5bb9d56310a3
anguyenguy <hdragon1501@gmail.com>
2022-07-19 15:10:02 +0700

    Initial commit

---
+++ lms/djangoapps/course_home_api/urls.py
@@ -0,0 +1,101 @@
+"""
+Contains all the URLs for the Course Home
+"""
+
+
+from django.conf import settings
+from django.urls import re_path
+
+from lms.djangoapps.course_home_api.course_metadata.views import CourseHomeMetadataView
+from openedx.features.funix_relative_date.views import FunixRelativeDatesTabView
+from lms.djangoapps.course_home_api.dates.views import DatesTabView
+from lms.djangoapps.course_home_api.outline.views import (
+    OutlineTabView, dismiss_welcome_message, save_course_goal, unsubscribe_from_course_goal_by_token,
+)
+from lms.djangoapps.course_home_api.progress.views import ProgressTabView
+from openedx.features.funix_goal.views import ( set_goal )
+
+# This API is a BFF ("backend for frontend") designed for the learning MFE. It's not versioned because there is no
+# guarantee of stability over time. It may change from one open edx release to another. Don't write any scripts
+# that depend on it.
+
+urlpatterns = []
+
+# URL for Course metadata content
+urlpatterns += [
+    re_path(
+        fr'course_metadata/{settings.COURSE_KEY_PATTERN}',
+        CourseHomeMetadataView.as_view(),
+        name='course-metadata'
+    ),
+]
+
+# Dates Tab URLs
+urlpatterns += [
+    re_path(
+        fr'dates/{settings.COURSE_KEY_PATTERN}',
+        DatesTabView.as_view(),
+        name='dates-tab'
+    ),
+]
+
+# Funix Goal URLs
+urlpatterns += [
+    re_path(
+        r'set_goal',
+        set_goal,
+        name='set-goal'
+    ),
+]
+
+# Funix Dates Tab URLs
+urlpatterns += [
+    re_path(
+        fr'dates-funix/{settings.COURSE_KEY_PATTERN}/(?P<student_id>[^/]+)',
+        FunixRelativeDatesTabView.as_view(),
+        name='funix-dates-tab-other-student'
+    ),
+    re_path(
+        fr'dates-funix/{settings.COURSE_KEY_PATTERN}',
+        FunixRelativeDatesTabView.as_view(),
+        name='funix-dates-tab'
+    ),
+]
+
+# Outline Tab URLs
+urlpatterns += [
+    re_path(
+        fr'outline/{settings.COURSE_KEY_PATTERN}',
+        OutlineTabView.as_view(),
+        name='outline-tab'
+    ),
+    re_path(
+        r'dismiss_welcome_message',
+        dismiss_welcome_message,
+        name='dismiss-welcome-message'
+    ),
+    re_path(
+        r'save_course_goal',
+        save_course_goal,
+        name='save-course-goal'
+    ),
+    re_path(
+        r'unsubscribe_from_course_goal/(?P<token>[^/]*)$',
+        unsubscribe_from_course_goal_by_token,
+        name='unsubscribe-from-course-goal'
+    ),
+]
+
+# Progress Tab URLs
+urlpatterns += [
+    re_path(
+        fr'progress/{settings.COURSE_KEY_PATTERN}/(?P<student_id>[^/]+)',
+        ProgressTabView.as_view(),
+        name='progress-tab-other-student'
+    ),
+    re_path(
+        fr'progress/{settings.COURSE_KEY_PATTERN}',
+        ProgressTabView.as_view(),
+        name='progress-tab'
+    ),
+]
---
+++ openedx/core/djangoapps/waffle_utils/testutils.py
@@ -0,0 +1,15 @@
+"""
+Test utilities for waffle utilities.
+"""
+
+# Can be used with FilteredQueryCountMixin.assertNumQueries() to blacklist
+# waffle tables. For example:
+#   QUERY_COUNT_TABLE_BLACKLIST = WAFFLE_TABLES
+#   with self.assertNumQueries(6, table_blacklist=QUERY_COUNT_TABLE_BLACKLIST):
+WAFFLE_TABLES = [
+    "waffle_utils_waffleflagcourseoverridemodel",
+    "waffle_utils_waffleflagorgoverridemodel",
+    "waffle_flag",
+    "waffle_switch",
+    "waffle_sample",
+]
